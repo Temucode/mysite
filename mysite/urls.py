00bc8a9a33d6373ac7f2f554ca191b187119b248
temucode <magvan.temuulen@gmail.com>
2024-01-08 15:27:32 +0800

    add django debug toolbar

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
